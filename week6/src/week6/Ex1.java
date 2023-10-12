9eb6a08c827f6443e93235d51bb37f351655d497
MediaLab <MediaLab@DESKTOP-9836970>
2023-10-12 14:52:34 +0900

    231012 수업시간

---
+++ week6/src/week6/Ex1.java
@@ -0,0 +1,24 @@
+package week6;
+
+class StaticSample{
+	public int n;
+	public void g() {
+		n = 20;
+	}
+	public void h() {
+		n = 30;
+	}
+	public static int m;
+	public static void f() {
+		n = 5;
+	}
+}
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
